052133b85e0529872c583e40f2231bde028315f2
sandy1480 <43827905+sandy1480@users.noreply.github.com>
2018-11-08 02:07:49 +0000

    Update db.js

--- db.js
+++ db.js
@@ -3,10 +3,9 @@ console.log("check db")
 var mysql = require('mysql')
 //var connectionQueryGetReport= 'SELECT * from tblEmployee WHERE EmpID =' + user_id;
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  port     : 3306,
-  user     : 'root',
-  password : 'Halifax@1234',
+  host     : 'ec2-35-178-199-197.eu-west-2.compute.amazonaws.com',
+  user     : 'TestUser',
+  password : 'PassWord',
   database : 'ninja_assignment2'
   });
 
